5b32ac08c6b02edbb143e58c25be15d18e7919b9
rebecalazarini <rebecalazarini2007@gmail.com>
2025-09-24 13:48:26 -0300

    altrações

--- api/src/controllers/seed.js
+++ api/src/controllers/seed.js
@@ -1,119 +1,108 @@
-const fs = require('fs');
-const csv = require('csv-parser');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
-// Função para inserir Perfis
 async function inserePerfis() {
-  const perfisData = [];
-
-  fs.createReadStream('./prisma/dados/perfis.csv')  // Caminho correto
-    .pipe(csv())
-    .on('data', (row) => {
-      perfisData.push({
-        id: Number(row.id),
-        perfil: row.perfil,
-      });
-    })
-    .on('end', async () => {
-      for (const perfil of perfisData) {
-        await prisma.perfil.create({
-          data: perfil,
-        });
-      }
-      console.log('Perfis importados');
-    });
+    const perfisData = fs.readFileSync(path.join(__dirname, '../../data/perfis.csv'), 'utf8');
+    const linhas = perfisData.split('\r\n');
+    const resultados = await Promise.all(linhas.map((linha, i) => {
+        if (i > 0 && linha) {
+            const [id, perfil] = linha.split(';');
+            return prisma.perfil.create({
+                data: {
+                    id: Number(id),
+                    perfil
+                }
+            });
+        }
+        return null;
+    }));
+    return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
-// Função para inserir Usuários
 async function insereUsuarios() {
-  const usuariosData = [];
-
-  fs.createReadStream('./prisma/dados/usuarios.csv')  // Caminho correto
-    .pipe(csv())
-    .on('data', (row) => {
-      usuariosData.push({
-        id: Number(row.id),
-        senha: row.senha,
-        perfil: Number(row.perfil),
-      });
-    })
-    .on('end', async () => {
-      for (const usuario of usuariosData) {
-        await prisma.usuario.create({
-          data: usuario,
-        });
-      }
-      console.log('Usuários importados');
-    });
+    const usuariosData = fs.readFileSync(path.join(__dirname, '../../data/usuarios.csv'), 'utf8');
+    const linhas = usuariosData.split('\r\n');
+    const resultados = await Promise.all(linhas.map((linha, i) => {
+        if (i > 0 && linha) {
+            const [id, senha, perfil] = linha.split(';');
+            return prisma.usuario.create({
+                data: {
+                    id: Number(id),
+                    senha,
+                    perfil: Number(perfil)
+                }
+            });
+        }
+        return null;
+    }));
+    return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
-// Função para inserir Equipamentos
 async function insereEquipamentos() {
-  const equipamentosData = [];
-
-  fs.createReadStream('./prisma/dados/equipamentos.csv')  // Caminho correto
-    .pipe(csv())
-    .on('data', (row) => {
-      equipamentosData.push({
-        id: Number(row.id),
-        equipamento: row.equipamento,
-        imagem: row.imagem,
-        descricao: row.descricao,
-        ativo: Number(row.ativo),
-        data: new Date(row.data) && !isNaN(new Date(row.data)) ? new Date(row.data) : null,  // Verificando se a data é válida
-      });
-    })
-    .on('end', async () => {
-      for (const equipamento of equipamentosData) {
-        await prisma.equipamento.create({
-          data: equipamento,
-        });
-      }
-      console.log('Equipamentos importados');
-    });
+    const equipamentosData = fs.readFileSync(path.join(__dirname, '../../data/equipamentos.csv'), 'utf8');
+    const linhas = equipamentosData.split('\r\n');
+    const resultados = await Promise.all(linhas.map((linha, i) => {
+        if (i > 0 && linha) {
+            const [id, equipamento, imagem, descricao, ativo, data] = linha.split(';');
+            return prisma.equipamento.create({
+                data: {
+                    id: Number(id),
+                    equipamento,
+                    imagem,
+                    descricao,
+                    ativo: Number(ativo),
+                    data: new Date(data)
+                }
+            });
+        }
+        return null;
+    }));
+    return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
-// Função para inserir Comentários
 async function insereComentarios() {
-  const comentariosData = [];
-
-  fs.createReadStream('./prisma/dados/comentarios.csv')  // Caminho correto
-    .pipe(csv())
-    .on('data', (row) => {
-      comentariosData.push({
-        id: Number(row.id),
-        comentario: row.comentario || '', // Garantir que o campo comentario não seja undefined
-        equipamento: Number(row.equipamento),
-        perfil: Number(row.perfil),
-        data: new Date(row.data) && !isNaN(new Date(row.data)) ? new Date(row.data) : null,  // Verificando se a data é válida
-      });
-    })
-    .on('end', async () => {
-      for (const comentario of comentariosData) {
-        await prisma.comentario.create({
-          data: comentario,
-        });
-      }
-      console.log('Comentários importados');
-    });
+    const comentariosData = fs.readFileSync(path.join(__dirname, '../../data/comentarios.csv'), 'utf8');
+    const linhas = comentariosData.split('\r\n');
+    const resultados = await Promise.all(linhas.map((linha, i) => {
+        if (i > 0 && linha) {
+            const [id, comentario, equipamento, perfil, data] = linha.split(';');
+            return prisma.comentario.create({
+                data: {
+                    id: Number(id),
+                    comentario,
+                    equipamento: Number(equipamento),
+                    perfil: Number(perfil),
+                    data: new Date(data)
+                }
+            });
+        }
+        return null;
+    }));
+    return resultados.filter(s => s !== null && s !== undefined).length;
 }
 
+// Função principal para semear os dados
 async function run(req, res) {
-  try {
-    await inserePerfis();
-    await insereUsuarios();
-    await insereEquipamentos();
-    await insereComentarios();
+    try {
+        await prisma.$connect();
+        await prisma.comentario.deleteMany();
+        await prisma.equipamento.deleteMany();
+        await prisma.usuario.deleteMany();
+        await prisma.perfil.deleteMany();
+        const perfis = await inserePerfis();
+        const usuarios = await insereUsuarios();
+        const equipamentos = await insereEquipamentos();
+        const comentarios = await insereComentarios();
+        await prisma.$disconnect();
+        res.json({ perfis, usuarios, equipamentos, comentarios });
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+        return res.status(500).json({ error: 'Erro ao conectar ao banco de dados' });
+    }
+};
 
-    res.json({ message: 'Importação dos dados concluída!' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro na importação dos dados' });
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-module.exports = { run };
+module.exports = {
+    run
+};
