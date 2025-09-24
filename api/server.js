3539616091165853a1fdb899b777763ec59d7c42
rebecalazarini <rebecalazarini2007@gmail.com>
2025-09-24 16:07:14 -0300

    tentando puxar os dados

--- api/server.js
+++ api/server.js
@@ -9,6 +9,20 @@ app.use(cors());
 app.use(routes);
 app.use('/', routes); 
 
+router.get('/equipamento', async (req, res) => {
+  try {
+    const equipamentos = await prisma.equipamento.findMany({
+      where: { ativo: 1 },
+      include: { comentarios: true }, // Pega os comentários relacionados
+      orderBy: { data: 'desc' }
+    });
+    res.json(equipamentos); // 🔹 aqui você já devolve JSON
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao buscar equipamentos' });
+  }
+});
+
 app.listen(3000, () => {
     console.log('API respondendo em http://localhost:3000')
 });
